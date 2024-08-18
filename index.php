52ec5714ed54893ff7ce1c965940c265b37cac2b
Hyunykai <140878207+Hyunykai@users.noreply.github.com>
2024-08-18 02:59:02 +0000

    테스트

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8"> <!--메타 카세트 설정-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="MUNIT과 함께 더욱 편리한 커뮤니케이션">
+        <meta name="description" content="sia">
         <meta name="google-site-verification" content="gh1bhPxgj2RNQotVFufA6H4n5WFBLsa5aJwrE_ZYxWY" /> <!--구글 노출 설정-->
         <meta name="naver-site-verification" content="c70fbada5f85926fb6e8c32296b79e048663f0d5" /> <!--네이버 노출 설정-->
         <link rel="icon" type="image/png" href="https://munit.me/image/munit_papi.png">
